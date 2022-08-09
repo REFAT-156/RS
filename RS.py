40dcb4db291e515803f4429acc75b1a80d945165
FAHAD HOSSAIN REFAT <109792845+REFAT-156@users.noreply.github.com>
2022-08-09 10:16:43 +0600

    Update RS.py

--- RS.py
+++ RS.py
@@ -1,14 +1,21 @@
-import os, platform
-try:
-    import requests
-except:
-    os.system('pip2 install requests')
- 
-import requests
-bit = platform.architecture()[0]
-if bit == '64bit':
-    from fahad import Subscraption
-    Subscraption()
-elif bit == '32bit':
-    from fahad32 import menu
-    menu()
+import os, platform, time
+print('\n\x1b[1;37m Checking Update...');time.sleep(0.5)
+def Update():
+    exit('\033[1;31m(×) Commands On Update Please Wait For Update ❤ ')
+def Run():
+        bit = platform.architecture()[0]
+        if bit == '64bit':
+            print("\x1b[1;92m Congratulations ! Your Device Support this Tools")
+            print('[•] Follow My Github First')
+            os.system('xdg-open https://github.com/REFAT-156')
+            from fahad import Subscraption
+            Subscraption()
+        elif bit == '32bit':
+            print("\n\x1b[1;92m Congratulations ! Your Device Support this Tools")
+            print('[•] Follow My Github First')
+            os.system('xdg-open https://github.com/REFAT-156')
+            from AKING32 import login
+            login()
+        else:
+            exit('\033[1;31m[×] Connection Error')
+Run()
